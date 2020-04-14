b97f6af565dd7ef8abb9c7fae0d5c4259596dc7a
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2020-04-14 10:03:02 +0200

    Add a dummy perf indicator

---
+++ performance.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def main():
+    result = {
+        "component": "tensorflow",
+        "name": "PiMatmul",
+        "@parameters": {
+            "dtype": "float32",
+        },
+        "@result": {
+            "rate": 31.21,
+            "elapsed": 10.210,
+        },
+        "tensorflow_buildinfo": None
+    }
+    json.dump(result, sys.stdout, indent=2)
+
+
+if __name__ == '__main__':
+    main()
