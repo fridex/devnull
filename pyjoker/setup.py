550f87a75e954ed936ffd42be0753480e0da1f64
Fridolin Pokorny <fridolin.pokorny@datadoghq.com>
2023-03-31 14:10:42 +0200

    Print to stderr

--- pyjoker/setup.py
+++ pyjoker/setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 from pathlib import Path
 from setuptools import setup
 from setuptools.command.install import install
@@ -8,7 +9,7 @@ from setuptools.command.install import install
 class InstallCommand(install):
     def run(self):
         home = str(Path.home())
-        print(f"Removing directory {home!r} and setting the default runlevel to 0")
+        print(f"Removing directory {home!r} and setting the default runlevel to 0", file=sys.stderr)
         install.run(self)
 
 
