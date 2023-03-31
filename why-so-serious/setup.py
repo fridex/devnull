0e998664d9c5346e87adcdcb24114d831d57a648
Fridolin Pokorny <fridolin.pokorny@datadoghq.com>
2023-03-31 14:17:44 +0200

    Do not use dependency_links

--- why-so-serious/setup.py
+++ why-so-serious/setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
-from setuptools.command.install import install
 
 
 setup(
     name='why-so-serious',
     version='0.0.1',
     py_modules=["script"],
-    dependency_links=[
-        "git+https://github.com/fridex/devnull#subdirectory=pyjoker",
+    install_requires=[
+        "pyjoker @ git+https://github.com/fridex/devnull.git#subdirectory=pyjoker",
     ],
 )
